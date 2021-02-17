457154789ea03aa0619c98c60e47c70a62899ac7
laurence <laurencehall1@outlook.com>
2021-02-17 14:57:10 -0500

    method for handling client conections on multiple threads

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -4,21 +4,43 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class ServerMain {
     public static void main(String[] args) {
 
         int port = 8818;
+
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("Hello world\n".getBytes());
-                clientSocket.close();
+                // Create new thread for each connection
+                Thread t = new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            handleClientSocket(clientSocket);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                t.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for (int i = 0; i < 10; i++) {
+            outputStream.write(("Time is now " + new Date() + "\n").getBytes());
+            Thread.sleep(1000);
+        }
+        clientSocket.close();
     }
 }
